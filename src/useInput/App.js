d34e531c01e2eb67193acbdef2817254149c5879
hoony0802 <hoony0802@naver.com>
2021-09-24 23:01:16 +0900

    Complete useInput Hook

---
+++ src/useInput/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e) => {
+    let willUpdate = true;
+
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+
+    if (willUpdate) {
+      setValue(e.target.value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+
+  const name = useInput("Mr.", maxLen);
+  // const [item, setItem] = useState(1);
+  // const incrementItem = () => setItem(item + 1);
+  // const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div className="App">
+      <h1>Hello{/* {item} */}</h1>
+      <input placeholder="Name" {...name} />
+      {/* <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button> */}
+    </div>
+  );
+};
+
+export default App;
