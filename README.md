e830bd68bbdb2135f833d60bbf1c22941dd3f5de
hoony0802 <hoony0802@naver.com>
2021-09-26 22:13:34 +0900

    Complete useConfirm Hook

--- README.md
+++ README.md
@@ -3,16 +3,25 @@
 ## Utilize useState
 
 1. useInput
-   input 태그에 사용할 수 있는 커스텀 훅
-   유효성 검사도 포함됨
+
+- input 태그에 사용할 수 있는 커스텀 훅
+
+* 유효성 검사도 포함됨
 
 2. useTabs
-   탭을 전환할 때 사용할 수 있는 커스텀 훅
+
+- 탭을 전환할 때 사용할 수 있는 커스텀 훅
 
 ## Utilize useEffect
 
 1. useTitle
-   title 태그 텍스트를 변경하는 커스텀 훅
+
+- title 태그 텍스트를 변경하는 커스텀 훅
 
 2. useClick
-   클릭 이벤트일 때 변경해주는 커스텀 훅
+
+- 클릭 이벤트일 때 변경해주는 커스텀 훅
+
+3. useConfirm
+
+- 유저에게 메세지를 보여주는 커스텀 훅
---
+++ src/useConfirm/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function") return;
+
+  const confirmAction = () => {
+    if (window.confirm(message)) callback();
+    else rejection();
+  };
+
+  return confirmAction;
+};
+
+const App = () => {
+  const deleteWorld = () => console.log("Deleting the world...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
+
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+};
+
+export default App;
