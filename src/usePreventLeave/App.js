4d6253af90254a66d9670d1398b396f3444b94d0
hoony0802 <hoony0802@naver.com>
2021-09-26 22:35:13 +0900

    Complete usePreventLeave Hook

---
+++ src/usePreventLeave/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>UnProtect</button>
+    </div>
+  );
+};
+
+export default App;
